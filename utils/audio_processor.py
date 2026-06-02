26c996d31b129c87425cc363f0bbff16b4c14245
Aditya Singh Parmar <adityaparmar04022006@gmail.com>
2026-06-02 14:14:44 +0530

    fix youtube 403 issue

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -10,17 +10,28 @@ os.makedirs(DOWNLOAD_DIR,exist_ok = True)
 def download_youtube_audio(url :str) ->str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
     ydl_opts = {
-        "format": "bestaudio/best",
-        "outtmpl": output_path,
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "wav",
-                "preferredquality": "192",
-            }
-        ],
-        "quiet": True,
-    }
+    "format": "bestaudio/best",
+    "outtmpl": output_path,
+    "quiet": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "geo_bypass": True,
+    "http_headers": {
+        "User-Agent": "Mozilla/5.0"
+    },
+    "extractor_args": {
+        "youtube": {
+            "player_client": ["android", "web"]
+        }
+    },
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "wav",
+            "preferredquality": "192",
+        }
+    ],
+  }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
